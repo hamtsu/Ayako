fbd381454e3e149f3bd553ae766b993b0e8c4d92
Hammy <68724706+hamtsu@users.noreply.github.com>
2022-01-28 22:46:53 +1100

    Commit

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Successfully logged into ${client.user.tag}.`);
 		client.user.setActivity('♡');
 		client.user.setStatus('dnd');
+
+		const Guilds = client.guilds.cache.map((guild) => guild);
+		const all_fetchedCommands = await Guilds[0].commands.fetch();
+		const purgeCommand = all_fetchedCommands.find(command => command.name === 'purge');
+		const purgeCommandId = purgeCommand.permissions.commandId;
 	},
 };
